b9ed10c2a4e399c01558703b98fbef905e61843c
caiomiliorini <69901831+caiomiliorini@users.noreply.github.com>
2020-09-14 00:19:41 -0300

    Add files via upload
    Aula04 desafio hardcore

--- aula04-debugger/js/main.js
+++ aula04-debugger/js/main.js
@@ -1,61 +1,106 @@
-//Definição das variáveis
-//Conexão com os elementos HTML
-//Campos de entrada
 let entradas = document.querySelectorAll('input');
-//Botão somar
 let btnSomar = document.querySelector('#btnSomar');
-//Parágrafo de resposta
-let resposta = document.querySelector('.resposta')
- 
-//Definindo o número 1 e número 2
+let btnSubtrair = document.querySelector('#btnSubtrair');
+let btnMultiplicar = document.querySelector('#btnMultiplicar');
+let btnDividir = document.querySelector('#btnDividir');
+let resposta = document.querySelector('.resposta');
 let num1 = entradas[0];
 let num2 = entradas[1];
- 
-//Funções para pegar os valores
 function pegarNum1() {
-   return num1.value;
+    return num1.value;
 }
-function pegarNum2() {
-   return num2.value;
+ function pegarNum2() {
+    return num2.value;
 }
- 
-//Verificando se as entradas estão vazias
 function verificarEntradasVazias() {
-   //Se num1 ou num2 estiverem vazios
-   if(pegarNum1() === '' || pegarNum2() === ''){
-       return true; //Retorna verdadeiro
-   }else{ //Senão
-       return false; //Retorna Falso
-   }
+    if(pegarNum1() === '' || pegarNum2() === ''){return true;}
+    else{return false;}
 }
- 
-//Função Somar
 function somar(){
-   if(verificarEntradasVazias()){
-       atualizaResposta();
-   }else{
-       atualizaResposta();
-   }
+    if(verificarEntradasVazias()){atualizaRespostaSomar();}
+    else{atualizaRespostaSomar();}
 }
- 
-//Escuta de evento click do mouse
+function subtrair(){
+        if(verificarEntradasVazias()){atualizaRespostaSubtrair();}
+        else{atualizaRespostaSubtrair();}
+}
+function multiplicar(){
+        if(verificarEntradasVazias()){atualizaRespostaMultiplicar();}
+        else{atualizaRespostaMultiplicar();}
+}
+function dividir(){
+        if(verificarEntradasVazias()){atualizaRespostaDividir();}
+        else{atualizaRespostaDividir();}
+}
 btnSomar.addEventListener('click',somar);
- 
-//Atualizar o campo de resposta de acordo com a função
-function atualizaResposta() {
-   if(verificarEntradasVazias()){
+btnSubtrair.addEventListener('click',subtrair);
+btnMultiplicar.addEventListener('click',multiplicar);
+btnDividir.addEventListener('click',dividir);
+function atualizaRespostaSomar() {
+    if(verificarEntradasVazias()){
         resposta.classList.remove('certa');
         resposta.classList.add('errada');
-        debugger; //Pressionar a tecla F12 no navegador
-       resposta.textContent = "Erro: Preencha os dois números!"
-   }else{
+        debugger;
+        resposta.textContent = "Erro: Preencha os dois números!"
+    }else{
         resposta.classList.remove('errada');
         resposta.classList.add('certa');
-       let numero1 = pegarNum1();
-       numero1 = parseFloat(numero1);//Convertendo para número Real
-       let numero2 = pegarNum2();
-       numero2 = parseFloat(numero2);//Convertendo para número Real
-       let soma = numero1 + numero2;
-       resposta.textContent = `${numero1} + ${numero2} = ${soma}`;
-   }
+        let numero1 = pegarNum1();
+        numero1 = parseFloat(numero1);
+        let numero2 = pegarNum2();
+        numero2 = parseFloat(numero2);
+        let soma = numero1 + numero2;
+        resposta.textContent = `${numero1} + ${numero2} = ${soma}`;
+    }
+}
+function atualizaRespostaSubtrair() {
+    if(verificarEntradasVazias()){
+        resposta.classList.remove('certa');
+        resposta.classList.add('errada');
+        debugger;
+        resposta.textContent = "Erro: Preencha os dois números!"
+    }else{
+        resposta.classList.remove('errada');
+        resposta.classList.add('certa');
+        let numero1 = pegarNum1();
+        numero1 = parseFloat(numero1);
+        let numero2 = pegarNum2();
+        numero2 = parseFloat(numero2);
+        let diferenca = numero1 - numero2;
+        resposta.textContent = `${numero1} - ${numero2} = ${diferenca}`;
+    }
+}
+function atualizaRespostaMultiplicar() {
+    if(verificarEntradasVazias()){
+        resposta.classList.remove('certa');
+        resposta.classList.add('errada');
+        debugger;
+        resposta.textContent = "Erro: Preencha os dois números!"
+    }else{
+        resposta.classList.remove('errada');
+        resposta.classList.add('certa');
+        let numero1 = pegarNum1();
+        numero1 = parseFloat(numero1);
+        let numero2 = pegarNum2();
+        numero2 = parseFloat(numero2);
+        let produto = numero1 * numero2;
+        resposta.textContent = `${numero1} * ${numero2} = ${produto}`;
+    }
+}
+function atualizaRespostaDividir() {
+    if(verificarEntradasVazias()){
+        resposta.classList.remove('certa');
+        resposta.classList.add('errada');
+        debugger;
+        resposta.textContent = "Erro: Preencha os dois números!"
+    }else{
+        resposta.classList.remove('errada');
+        resposta.classList.add('certa');
+        let numero1 = pegarNum1();
+        numero1 = parseFloat(numero1);
+        let numero2 = pegarNum2();
+        numero2 = parseFloat(numero2);
+        let quociente = numero1 / numero2;
+        resposta.textContent = `${numero1} / ${numero2} = ${quociente}`;
+    }
 }
